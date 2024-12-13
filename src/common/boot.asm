64f2d4cb73dbe9fe435fa43f319096b3974b68c4
Wrench56 <markdevenyidev@gmail.com>
2024-12-13 00:52:48 -0500

    refactor(init): merge boot and author messages in `boot.asm`

--- src/common/boot.asm
+++ src/common/boot.asm
@@ -2,14 +2,12 @@
 %include "includes/common/macros.inc"
 
 section .data
-    header_msg          db "[     ] ==========================================", 10
+    header_msg          db "[     ] Hello from oxnag!", 10, \
+                           "[     ] ==========================================", 10, \
+                           "[     ] Made by:", 10, \
+                           "[     ]  > Mark Devenyi <markdevenyidev@gmail.com>", 10, \
+                           "[     ] ==========================================", 10
     header_msg_len      equ $ - header_msg
-    boot_msg            db "[     ] Hello from oxnag!", 10
-    boot_msg_len        equ $ - boot_msg
-    author_msg          db "[     ] Made by:", 10
-    author_msg_len      equ $ - author_msg
-    author1             db "[     ]  > Mark Devenyi <markdevenyidev@gmail.com>", 10
-    author1_len         equ $ - author1
 
 
 %ifidn __OUTPUT_FORMAT__, win64
@@ -27,10 +25,6 @@ boot_process:
     os_spec_boot
 
     ; Bootup message
-    log             boot_msg, boot_msg_len
-    log             header_msg, header_msg_len
-    log             author_msg, author_msg_len
-    log             author1, author1_len
     log             header_msg, header_msg_len
 
     leave
